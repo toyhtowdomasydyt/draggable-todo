8d86afe61e74d94e143b0afe33bb6cfee3e42f70
Joel D <imaginethere.snoie@gmail.com>
2021-08-21 18:08:50 +0300

    Update styles only with snapshot object

--- src/App.js
+++ src/App.js
@@ -7,25 +7,7 @@ import Column from "./components/Column";
 const App = () => {
     const [state, setState] = useState(initalData);
 
-    const onDragStart = () => {
-        document.body.style.color = 'red';
-        document.body.style.transition = 'background-color .2s ease';
-    };
-
-    const onDragUpdate = update => {
-        const {destination} = update;
-
-        const opacity = destination
-            ? destination.index / Object.keys(state.tasks).length
-            : 0;
-
-        document.body.style.backgroundColor = `rgba(153, 141, 217, ${opacity}`;
-    }
-
     const onDragEnd = result => {
-        document.body.style.color = 'inherit';
-        document.body.style.backgroundColor = '#444444';
-
         const {destination, source, draggableId} = result;
 
         if (!destination) return;
@@ -52,8 +34,6 @@ const App = () => {
     return (
         <DragDropContext
             onDragEnd={onDragEnd}
-            onDragStart={onDragStart}
-            onDragUpdate={onDragUpdate}
         >
             {state.columnOrder.map(columnId => {
                 const column = state.columns[columnId];
