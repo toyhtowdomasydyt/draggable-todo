e657de6aa440b7409da07ca09510b4a43c2f0df9
Joel D <imaginethere.snoie@gmail.com>
2021-08-21 16:48:58 +0300

    Create initial-data.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-const App = () => (
-    <div>
-        <h1>It's alive</h1>
-    </div>
-);
+import {initalData} from "./initial-data";
+import Column from "./components/Column";
+
+const App = () => {
+    const [state, setState] = useState(initalData);
+
+    return (
+        <div>
+            {state.columnOrder.map(columnId => {
+                const column = state.columns[columnId];
+                const tasks = column.taskIds.map(taskId => state.tasks[taskId]);
+
+                return <Column key={column.id} column={column} tasks={tasks}/>
+            })}
+        </div>
+    );
+};
 
 export default App;
---
+++ src/initial-data.js
@@ -0,0 +1,16 @@
+export const initalData = {
+    tasks: {
+        "task-1": {id: "task-1", content: "Task 1"},
+        "task-2": {id: "task-2", content: "Task 2"},
+        "task-3": {id: "task-3", content: "Task 3"},
+        "task-4": {id: "task-4", content: "Task 4"},
+    },
+    columns: {
+        "column-1": {
+            id: "column-1",
+            title: "Todo",
+            taskIds: ["task-1", "task-2", "task-3", "task-4"]
+        },
+    },
+    columnOrder: ["column-1"],
+};
