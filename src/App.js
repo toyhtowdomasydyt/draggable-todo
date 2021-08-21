c70a9511de375d245d94c4b05648263982d979fa
Joel D <imaginethere.snoie@gmail.com>
2021-08-21 17:25:25 +0300

    Add simple DND

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react';
+import {DragDropContext} from "react-beautiful-dnd";
 
 import {initalData} from "./initial-data";
 import Column from "./components/Column";
@@ -7,14 +8,14 @@ const App = () => {
     const [state, setState] = useState(initalData);
 
     return (
-        <div>
+        <DragDropContext>
             {state.columnOrder.map(columnId => {
                 const column = state.columns[columnId];
                 const tasks = column.taskIds.map(taskId => state.tasks[taskId]);
 
                 return <Column key={column.id} column={column} tasks={tasks}/>
             })}
-        </div>
+        </DragDropContext>
     );
 };
 
--- src/components/Column.jsx
+++ src/components/Column.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import Task from "./Task";
+import {Droppable} from "react-beautiful-dnd";
 
 const Column = ({column, tasks}) => {
     const Container = styled.div`
       margin: 10px;
-      border-radius: 5px;
+      border-radius: 3px;
       background-color: #fff;
-      max-width: fit-content;
     `;
     const Title = styled.h3`
       padding: 8px
@@ -18,9 +19,19 @@ const Column = ({column, tasks}) => {
     return (
         <Container className="column">
             <Title>{column.title}</Title>
-            <TaskList>
-                {tasks.map(task => <div key={task.id}>{task.content}</div>)}
-            </TaskList>
+            <Droppable droppableId={column.id}>
+                {(provided) => (
+                    <TaskList
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                    >
+                        {tasks.map((task, index) => (
+                            <Task key={task.id} task={task} index={index}/>)
+                        )}
+                        {provided.placeholder}
+                    </TaskList>
+                )}
+            </Droppable>
         </Container>
     );
 };
